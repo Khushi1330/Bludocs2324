b325825abbdea83aa68bc92c551e77f0e3e687e5
Khushi1330 <Khushipandey1330@gmail.com>
2025-07-19 19:23:03 +0530

    Changes for Ec2 hosting

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ console.log("Loaded ENV:", {
 import app from "./app.js";
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   
 
